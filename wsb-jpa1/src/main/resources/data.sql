def8e2fec904c05bea9b99401a9ea84e824f4847
DanielakDaniel <wrx76044@student.wroclaw.merito.pl>
2025-06-15 18:24:07 +0200

    Update data.sql

--- wsb-jpa1/src/main/resources/data.sql
+++ wsb-jpa1/src/main/resources/data.sql
@@ -3,16 +3,16 @@ insert into address (id, address_line1, address_line2, city, postal_code)
 insert into address (id, address_line1, address_line2, city, postal_code)
             values (902, 'xxx', 'yyy', 'city', '60-400');
 insert into patient (id, first_name, last_name, telephone_number, email, patient_number, date_of_birth)
-            values (1, 'Di', 'Di', '987654321', 'didi@pacjent.pl', '13', '2001-01-01');
+            values (100, 'Di', 'Di', '987654321', 'didi@pacjent.pl', '13', '2001-01-01');
 insert into doctor (id, first_name, last_name, telephone_number, email, doctor_number, specialization)
-            values (11, 'Da', 'Da', '123456789', 'dada@pacjent.pl', '7', 'GP');
+            values (111, 'Da', 'Da', '123456789', 'dada@pacjent.pl', '7', 'GP');
 insert into visit (id, description, time, doctor_id, patient_id)
-            values (1, 'Wizyta kontrolna', '2025-06-16 08:30:00', 11, 1);
+            values (100, 'Wizyta kontrolna', '2025-06-16 08:30:00', 111, 100);
 insert into visit (id, description, time, doctor_id, patient_id)
-            values (2, 'Wizyta kontrolna', '2025-06-16 10:00:00', 11, 1);
+            values (101, 'Wizyta kontrolna', '2025-06-16 10:00:00', 111, 100);
 insert into medical_treatment (id, description, type, visit_id)
-            values (1, 'Wizyta kontrolna', 'USG', 1);
+            values (100, 'Wizyta kontrolna', 'USG', 100);
 insert into patient_to_address (patient_id, address_id)
-            values (1, 901);
+            values (100, 901);
 insert into doctor_to_address (doctor_id, address_id)
-            values (11, 902);
+            values (111, 902);
